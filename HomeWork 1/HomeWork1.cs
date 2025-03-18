68774693c06c3e6275e5b16f275c136f82ad6c47
Oleh-Shpyta <madmangambit0@gmail.com>
2025-03-18 21:25:07 +0100

    Garbage collection

--- HomeWork 1/HomeWork1.cs
+++ HomeWork 1/HomeWork1.cs
@@ -1,59 +1,110 @@
-﻿using System;
-class Program 
+﻿/* П"єса
+using System;
+class Program
 {
-    static void Main(string[] args) 
+    static void Main()
     {
-        float one, two, result;
-        char sign;
-
-        Console.WriteLine("Добро пожалувати в Калькулятор");
-        Console.WriteLine("Введіть перше число: ");
-        one = Convert.ToSingle(Console.ReadLine());
-        Console.WriteLine("Ввдіть знак (+, -, *, /): ");
-        string input = Console.ReadLine();
-        if (input.Length == 1)
+        using (Play play1 = new Play("Гамлет", "Вільям Шекспір", "Трагедія", 1603))
         {
-            sign = input[0];
-        }
-        else 
-        {
-            Console.WriteLine("Не коректно введений знак! ");
-            return;
-        }
-            Console.WriteLine("Введіть друге число: ");
-        two = Convert.ToSingle(Console.ReadLine());
-
-        switch (sign) 
-        {
-            case '+': result = one+ two ;
-                Console.WriteLine($"Сума ваших чисел дорівнює: {result} ");
-            break;
-
-            case '-':
-                result = one - two;
-                Console.WriteLine($"Різниця ваших чисел дорівнює: {result} ");
-            break;
-
-            case '*':
-                result = one * two ;
-                Console.WriteLine($"Множення ваших чисел дорівнює: {result} ");
-            break;
-
-            case '/':
-                if (two == 0)
-                    Console.WriteLine("Ділити на 0 неможливо! ");
-
-                else
-                {
-                    result = one / two;
-                    Console.WriteLine($"Ділення ваших чисел дорівнює: {result} ");
-                }
-            break;
-        default: Console.WriteLine("Ви ввели недопустимий символ! ");
-                Console.WriteLine("Для виходу настисніть будь-яку клавішу ");
-                break;
-
+            play1.DisplayInfo();
         }
 
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
+class Play : IDisposable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    private bool disposed = false;
+
+    public Play(string Title, string Author, string Genre, int Year)
+    {
+        this.Title = Title;
+        this.Author = Author;
+        this.Genre = Genre;
+        this.Year = Year;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"П'єса: {Title}, Автор: {Author}, Жанр: {Genre}, Рік: {Year}");
+    }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine($"Dispose: Закриття п'єси \"{Title}\".");
+            }
+            disposed = true;
+        }
+    }
+    ~Play()
+    {
+        Dispose(false);
+        Console.WriteLine($"Деструктор: Видалення п'єси \"{Title}\".");
+    }
+}
+*/
+//Магазин 
+using System;
+class Program
+{
+    static void Main()
+    {
+        using (Store store1 = new Store("АТБ", "Житомир, вул. Житомирська, 1", "Продовольчий"))
+        {
+            store1.DisplayInfo();
+        }
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
+class Store : IDisposable
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string StoreType { get; set; }
+    private bool disposed = false;
+
+    public Store(string name, string address, string storeType)
+    {
+        Name = name;
+        Address = address;
+        StoreType = storeType;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Магазин: {Name}, Адреса: {Address}, Тип: {StoreType}");
+    }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine($"Dispose: Закриття магазину \"{Name}\".");
+            }
+            disposed = true;
+        }
+    }
+    ~Store()
+    {
+        Dispose(false);
+        Console.WriteLine($"Деструктор: Видалення магазину \"{Name}\".");
     }
 }
